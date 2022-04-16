9f827623859e4c3a2970368557b269f717d9b87e
lsx <lsx7@sina.com>
2022-04-17 02:01:18 +0800

    静态编译、gcc+upx压缩、32位编译

--- easy_dist.bat
+++ easy_dist.bat
@@ -1,4 +1,6 @@
+@echo off
 taskkill /f /im qrcode.exe
-g++ -o qrcode.exe qrcodegen.cpp main.cpp -DWINVER=0x0A00 -DUNICODE  -mwindows -municode
+g++ -o qrcode.exe qrcodegen.cpp main.cpp -m32 -s -static -DWINVER=0x0A00 -DUNICODE  -mwindows -municode
+upx -9 qrcode.exe
 start qrcode.exe
 pause
