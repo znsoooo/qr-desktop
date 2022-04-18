314e6adba1c8b8a33a67c65e74565d0acf60fba0
lsx <lsx7@sina.com>
2022-04-18 19:26:43 +0800

    v0.1.3

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ using std::tuple;
 using namespace std;
 using namespace qrcodegen;
 
-#define    QR_TITLE       L"QR Desktop 0.1.2"
+#define    QR_TITLE       L"QR Desktop 0.1.3"
 const int  QR_PAGE_SIZE = 2000; // 1个汉字占2个字节
 
 
