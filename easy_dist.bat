fe53e469ec749841cce01a6af1952e5666e61ed1
lsx <11313213@qq.com>
2022-07-26 21:41:23 +0800

    Refactor: main.cpp -> main.c

--- easy_dist.bat
+++ easy_dist.bat
@@ -1,7 +1,7 @@
 @echo off
 taskkill /f /im qrcode.exe
 windres -i icon.rc -o icon.o -F pe-i386
-gcc main.cpp qrcodegen.c icon.o -o qrcode.exe -m32 -s -static -mwindows
+gcc main.c qrcodegen.c icon.o -o qrcode.exe -m32 -s -static -mwindows
 upx -9 qrcode.exe
 start qrcode.exe
 pause
